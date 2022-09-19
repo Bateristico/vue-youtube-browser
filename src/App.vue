4866e6c73d91d48919d65f5a681ded5c00e8e46c
Camilo Saa <camilo.saa@gmail.com>
2022-09-19 21:38:12 +0200

    add google api key and axios fetch

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,29 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
 
 export default {
   name: 'App',
   components: { SearchBar },
   methods: {
-    onTermChange(searchTerm) {
-      console.log('parent log', searchTerm);
+    async onTermChange(searchTerm) {
+      try {
+        const response = await axios.get('https://googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        });
+
+        console.log('youtube api response', response);
+      } catch (error) {
+        console.log('Error while calling google api: ', error);
+      }
     }
   }
 };
