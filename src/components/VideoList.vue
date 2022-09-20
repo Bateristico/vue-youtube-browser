239b1e24b42fce6d80b3d855db8f5922389f7f6c
Camilo Saa <camilo.saa@gmail.com>
2022-09-20 20:14:51 +0200

    add v-for directive for video list item

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,15 +1,15 @@
 <template>
   <ul>
-    VIDEO LIST
-    {{
-      videos
-    }}
+    <video-list-item v-for="video in videos" :key="video"></video-list-item>
   </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
   name: 'VideoList',
+  components: { VideoListItem },
   props: {
     videos: {
       type: Array
